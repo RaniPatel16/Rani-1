683c67528c8f25e3488d4aae3c1d5831fb467781
Rani Patel <ranip161206@gmail.com>
2025-09-04 08:07:07 +0530

    exe2.js

--- gitfolder3/exe2.js
+++ gitfolder3/exe2.js
@@ -1,1 +1,12 @@
 sed -i 's/Hello, /Welcome, /' app.js
+Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch main
+# Changes to be committed:
+#	modified:   Rani-1
+#
+# Changes not staged for commit:
+#	modified:   Rani-1 (modified content)
+#	modified:   Rani-2 (modified content)
+#
